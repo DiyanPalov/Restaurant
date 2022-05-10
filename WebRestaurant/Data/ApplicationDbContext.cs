32905549c497448d9deb9446966ae41302db9f1d
DiyanPalov <ikokos@abv.bg>
2022-05-10 21:29:29 +0300

    Add project files.

---
+++ WebRestaurant/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebRestaurant.Model;
+
+namespace WebRestaurant.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
