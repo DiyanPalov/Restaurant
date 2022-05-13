d71a404169052301c052cbe1b5e19dfaf24a4682
DiyanPalov <ikokos@abv.bg>
2022-05-13 11:24:52 +0300

    Edit button added

---
+++ WebRestaurant/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebRestaurant.Data;
+using WebRestaurant.Model;
+
+namespace WebRestaurant.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                 _db.Category.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
