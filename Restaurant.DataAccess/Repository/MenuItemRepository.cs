befa824955890c5b900ca4e9ba6e32834cafcbf9
DiyanPalov <ikokos@abv.bg>
2022-08-23 17:30:02 +0300

    Adding MneuItem repository

---
+++ Restaurant.DataAccess/Repository/MenuItemRepository.cs
@@ -0,0 +1,30 @@
+﻿using Restaurant.DataAccess.Data;
+using Restaurant.DataAccess.Repository.IRepository;
+using Restaurant.Models;
+
+namespace Restaurant.DataAccess.Repository
+{
+    public class MenuItemRepository : Repository<MenuItem>, IMenuItemRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MenuItemRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(MenuItem obj)
+        {
+            var objFromDb = _db.MenuItem.FirstOrDefault(u => u.Id == obj.Id);
+            objFromDb.Name = obj.Name;
+            objFromDb.Description = obj.Description;
+            objFromDb.Price = obj.Price;
+            objFromDb.CategoryId = obj.CategoryId;
+            objFromDb.FoodTypeId = obj.FoodTypeId;
+            if (objFromDb.Image!=null)
+            {
+                objFromDb.Image = obj.Image;
+            }
+        }
+    }
+}
